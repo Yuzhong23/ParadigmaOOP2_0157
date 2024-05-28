fe456eb72035462fba94273cc9caf9505db5c4c0
Yuzhong23 <xixixixifatur@gmail.com>
2024-05-28 10:18:17 +0700

    Membuat keterangan

--- ParadigmaOOPpart2_0157.cpp
+++ ParadigmaOOPpart2_0157.cpp
@@ -4,7 +4,12 @@ using namespace std;
 class seseorang {
 public:
     virtual void pesan() = 0;
+    //virtual void pesan()
+    // {
+    //    cout << "pesan dari seseorang" << endl;
+    // }
 };
+
 
 
 int main()
