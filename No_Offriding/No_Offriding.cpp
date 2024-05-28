ce2ec2c8b8b1a28ef7da6e1869fef1454f289104
Yuzhong23 <xixixixifatur@gmail.com>
2024-05-28 10:33:38 +0700

    Prosedure awal

---
+++ No_Offriding/No_Offriding.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+// class parent 
+// tambahkan final sesudah nama class
+// untuk mencegah adanya overriding
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
